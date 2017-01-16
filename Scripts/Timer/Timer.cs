d78256882464f346f0a6d657c518a5cd9e148f20
ted <ted@randomidea.com>
2017-01-16 10:59:14 +0800

    - Add TimerManager.

---
+++ Scripts/Timer/Timer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TEDCore.Timer
+{
+	public class Timer
+	{
+		public float Duration { get { return _duration; } }
+		public object Data { get { return _data; } }
+		public bool HaveFinished { get { return _haveFinished; } }
+
+		private float _duration;
+		private Action<object> _onTimerFinished;
+		private object _data;
+		private bool _haveFinished = false;
+
+		public Timer(float duration, Action<object> onTimerFinished = null, object data = null)
+		{
+			_duration = duration;
+			_onTimerFinished = onTimerFinished;
+			_data = data;
+		}
+
+
+		public void Update(float deltaTime)
+		{
+			_duration -= deltaTime;
+
+			if (_duration <= 0 && !_haveFinished)
+			{
+				_haveFinished = true;
+
+				if (null != _onTimerFinished)
+				{
+					_onTimerFinished (_data);
+				}
+			}
+		}
+	}
+}
---
+++ Scripts/Timer/TimerManager.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TEDCore.Timer
+{
+	public class TimerManager
+	{
+		private List<Timer> _timers;
+		private bool _pause = false;
+		private Timer _timerCache;
+		private float _lastRealTime = 0;
+
+		public TimerManager()
+		{
+			_timers = new List<Timer> ();
+		}
+
+
+		public void AddTimer(Timer timer)
+		{
+			_timers.Add (timer);
+		}
+
+
+		public void RemoveTimer(Timer timer)
+		{
+			_timers.Remove (timer);
+		}
+
+
+		public Timer Schedule(float duration, Action<object> onTimerFinished = null, object data = null)
+		{
+			Timer newTimer = new Timer (duration, onTimerFinished, data);
+			AddTimer (newTimer);
+			return newTimer;
+		}
+
+
+		public void Update()
+		{
+			_lastRealTime = Time.realtimeSinceStartup - _lastRealTime;
+
+			if (_timers.Count != 0 && !_pause)
+			{
+				for (int cnt = 0; cnt < _timers.Count; cnt++)
+				{
+					_timerCache = _timers [cnt];
+					_timerCache.Update ();
+
+					if(_timerCache.HaveFinished)
+					{
+						RemoveTimer (_timerCache);
+					}
+				}
+			}
+
+			_lastRealTime = Time.realtimeSinceStartup;
+		}
+
+
+		public void Pause()
+		{
+			_pause = true;
+		}
+
+
+		public void Resume()
+		{
+			_pause = false;
+		}
+	}
+}
