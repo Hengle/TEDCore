4e192be31f25c9c8b67ee9228b3fe55e87ad601c
ted <ted@randomidea.com>
2017-01-17 23:13:13 +0800

    - Implemented State and Task generate script.

---
+++ Tools/StateTool/Editor/StateTool.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class StateTool : EditorWindow
+{
+	private static string TEMPLATE_PATH = "Assets/TEDCore/Tools/StateTool/Template_State.txt";
+	private static string GENERATE_SCRIPT_PATH = Application.dataPath + "/Scripts/States/";
+	private static string STATE = "State";
+
+	private string _stateName;
+
+	[MenuItem("TEDTools/State/Generate")]
+	public static void ShowWindow()
+	{
+		EditorWindow.GetWindow(typeof(StateTool));
+	}
+
+	private void OnGUI()
+	{
+		GUILayout.BeginVertical ();
+		_stateName = EditorGUILayout.TextField ("State Name", _stateName);
+
+		if (!string.IsNullOrEmpty (_stateName))
+		{
+			if (File.Exists (GENERATE_SCRIPT_PATH + _stateName + STATE + ".cs"))
+			{
+				GUILayout.TextField (_stateName + STATE + ".cs have already exist.");
+			}
+			else if (GUILayout.Button ("Generate"))
+			{
+				string template = GetTemplate (TEMPLATE_PATH);
+				template = template.Replace ("$StateName", _stateName + STATE);
+
+				GenerateScript (_stateName + STATE, template);
+
+				AssetDatabase.Refresh ();
+			}
+		}
+
+		GUILayout.EndVertical ();
+	}
+
+	private static string GetTemplate(string path)
+	{
+		TextAsset txt = (TextAsset)AssetDatabase.LoadAssetAtPath(path, typeof(TextAsset));
+
+		return txt.text;
+	}
+
+	private static void GenerateScript(string fileName, string content)
+	{
+		fileName = GENERATE_SCRIPT_PATH + fileName + ".cs";
+
+		if (!Directory.Exists (GENERATE_SCRIPT_PATH))
+		{
+			Directory.CreateDirectory (GENERATE_SCRIPT_PATH);
+		}
+
+		if (File.Exists(fileName))
+		{
+			File.Delete (fileName);
+		}
+
+		StreamWriter sr = File.CreateText(fileName);
+		sr.WriteLine (content);
+		sr.Close();
+	}
+}
---
+++ Tools/TaskTool/Editor/TaskTool.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class TaskTool : EditorWindow
+{
+	private static string TEMPLATE_PATH = "Assets/TEDCore/Tools/TaskTool/Template_Task.txt";
+	private static string GENERATE_SCRIPT_PATH = Application.dataPath + "/Scripts/Tasks/";
+	private string _taskName;
+
+	[MenuItem("TEDTools/Task/Generate")]
+	public static void ShowWindow()
+	{
+		EditorWindow.GetWindow(typeof(TaskTool));
+	}
+
+	private void OnGUI()
+	{
+		GUILayout.BeginVertical ();
+		_taskName = EditorGUILayout.TextField ("Task Name", _taskName);
+
+		if (!string.IsNullOrEmpty (_taskName))
+		{
+			if (File.Exists (GENERATE_SCRIPT_PATH + _taskName + "Task.cs"))
+			{
+				GUILayout.TextField (_taskName + "Task.cs have already exist.");
+			}
+			else if (GUILayout.Button ("Generate"))
+			{
+				string template = GetTemplate (TEMPLATE_PATH);
+				template = template.Replace ("$TaskName", _taskName + "Task");
+
+				GenerateScript (_taskName + "Task", template);
+
+				AssetDatabase.Refresh ();
+			}
+		}
+
+		GUILayout.EndVertical ();
+	}
+
+	private static string GetTemplate(string path)
+	{
+		TextAsset txt = (TextAsset)AssetDatabase.LoadAssetAtPath(path, typeof(TextAsset));
+
+		return txt.text;
+	}
+
+	private static void GenerateScript(string fileName, string content)
+	{
+		fileName = GENERATE_SCRIPT_PATH + fileName + ".cs";
+
+		if (!Directory.Exists (GENERATE_SCRIPT_PATH))
+		{
+			Directory.CreateDirectory (GENERATE_SCRIPT_PATH);
+		}
+
+		if (File.Exists(fileName))
+		{
+			File.Delete (fileName);
+		}
+
+		StreamWriter sr = File.CreateText(fileName);
+		sr.WriteLine (content);
+		sr.Close();
+	}
+}
