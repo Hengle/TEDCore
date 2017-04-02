98f9dd2ba24e0287b9b48d1751bbc300a46003f2
ted <ted10401@gmail.com>
2017-04-02 13:47:33 +0800

    - Refractor the template script tool.

---
+++ Tools/TemplateScriptTool/Editor/ReplaceTemplateScriptNameAction.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.ProjectWindowCallback;
+using System.IO;
+
+public class ReplaceTemplateScriptNameAction : EndNameEditAction
+{
+	public override void Action (int instanceId, string pathName, string resourceFile)
+	{
+		string text = File.ReadAllText (resourceFile);
+		string scriptName = Path.GetFileNameWithoutExtension (pathName);
+
+		text = text.Replace ("$SCRIPTNAME", scriptName);
+
+		StreamWriter sr = File.CreateText(pathName);
+		sr.WriteLine (text);
+		sr.Close();
+
+		AssetDatabase.ImportAsset (pathName);
+		ProjectWindowUtil.ShowCreatedAsset (AssetDatabase.LoadAssetAtPath<TextAsset>(pathName));
+		AssetDatabase.Refresh ();
+	}
+}
---
+++ Tools/TemplateScriptTool/Editor/TemplateScriptTool.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.IO;
+
+public class TemplateScriptTool
+{
+	[MenuItem ("Assets/Create/C# Scripts/Empty State")]
+	private static void CreateEmptyState ()
+	{
+		CreateTemplateScript ("TEDCore/Tools/TemplateScriptTool/Templates/TemplateState.txt", "NewState.cs");
+	}
+
+	[MenuItem ("Assets/Create/C# Scripts/Empty Task")]
+	private static void CreateEmptyTask ()
+	{
+		CreateTemplateScript ("TEDCore/Tools/TemplateScriptTool/Templates/TemplateTask.txt", "NewTask.cs");
+	}
+
+	private static void CreateTemplateScript(string resourcePath, string pathName)
+	{
+		string resourceFile = Path.Combine (Application.dataPath, resourcePath);
+
+		Texture2D csIcon = EditorGUIUtility.IconContent ("cs Script Icon").image as Texture2D;
+
+		ReplaceTemplateScriptNameAction endNameEditAction = ScriptableObject.CreateInstance<ReplaceTemplateScriptNameAction> ();
+
+		ProjectWindowUtil.StartNameEditingIfProjectWindowExists (0, endNameEditAction, pathName, csIcon, resourceFile);
+	}
+}
