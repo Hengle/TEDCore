9c44dcc51d2628985533eea73638463a98dbe5a6
ted <ted10401@gmail.com>
2018-10-30 17:49:13 +0800

    Add UnitTesting for ReadOnly attribute.

---
+++ Scripts/UnitTesting/UnitTesting_ReadOnly.cs
@@ -0,0 +1,8 @@
+﻿using TEDCore.UnitTesting;
+using UnityEngine;
+
+public class UnitTesting_ReadOnly : BaseUnitTesting
+{
+    [ReadOnly] [SerializeField] private int m_int;
+    [ReadOnly] [SerializeField] private GameObject m_gameObject;
+}
