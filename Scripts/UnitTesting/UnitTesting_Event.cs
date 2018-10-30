8df0a2a36f2a11ffa8b8407d6dea678e5b867d9d
ted <ted10401@gmail.com>
2018-10-30 10:17:20 +0800

    Add UnitTesting for event manager.

---
+++ Scripts/UnitTesting/UnitTesting_Event.cs
@@ -0,0 +1,76 @@
+﻿using TEDCore.UnitTesting;
+using TEDCore.Event;
+using UnityEngine;
+
+public class UnitTesting_Event : BaseUnitTesting
+{
+    public enum TestEnum
+    {
+        Update,
+        FixedUpdate,
+        LateUpdate
+    }
+
+    [SerializeField] private int m_updateCount;
+    [SerializeField] private int m_fixedUpdateCount;
+    [SerializeField] private int m_lateUpdateCount;
+    private EventListener m_eventListener;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 9999;
+        QualitySettings.vSyncCount = 0;
+        m_eventListener = new EventListener();
+    }
+
+    [TestButton]
+    public void RegisterUpdateEvent()
+    {
+        m_eventListener.ListenForEvent(TestEnum.Update.GetHashCode(), OnUpdate);
+    }
+
+    [TestButton]
+    public void RegisterFixedUpdateEvent()
+    {
+        m_eventListener.ListenForEvent(TestEnum.FixedUpdate.GetHashCode(), OnFixedUpdate);
+    }
+
+    [TestButton]
+    public void RegisterLateUpdateEvent()
+    {
+        m_eventListener.ListenForEvent(TestEnum.LateUpdate.GetHashCode(), OnLateUpdate);
+    }
+
+    private void Update()
+    {
+        EventManager.Instance.SendEvent(TestEnum.Update.GetHashCode());
+    }
+
+    private void FixedUpdate()
+    {
+        EventManager.Instance.SendEvent(TestEnum.FixedUpdate.GetHashCode());
+    }
+
+    private void LateUpdate()
+    {
+        EventManager.Instance.SendEvent(TestEnum.LateUpdate.GetHashCode());
+    }
+
+    private EventResult OnUpdate(object eventData)
+    {
+        m_updateCount++;
+        return null;
+    }
+
+    private EventResult OnFixedUpdate(object eventData)
+    {
+        m_fixedUpdateCount++;
+        return null;
+    }
+
+    private EventResult OnLateUpdate(object eventData)
+    {
+        m_lateUpdateCount++;
+        return null;
+    }
+}
