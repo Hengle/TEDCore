c45a0c4cbbb5f9f6cdc6cde8b7021490c8ac9d3e
ted <ted10401@gmail.com>
2018-11-07 09:18:26 +0800

    Add the new drag and sizing helper.

---
+++ Plugins/Debugger/DebuggerDragHelper.cs
@@ -0,0 +1,157 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+namespace TEDCore.Debugger
+{
+    public class DebuggerDragHelper : MonoBehaviour
+    {
+        [SerializeField] private bool m_moveToBorder = true;
+        [SerializeField] private float m_duration = 0.1f;
+
+        private DebuggerSizeHelper m_debuggerSizeHelper;
+        private RectTransform m_rectTransform;
+        private Vector2 m_fullWindowSize;
+
+        #region Drag
+        private Vector2 m_startPosition;
+        private Vector2 m_targetPosition;
+        private float m_moveTimer;
+        private bool m_isMoving;
+        #endregion
+
+        private void Awake()
+        {
+            m_debuggerSizeHelper = GetComponent<DebuggerSizeHelper>();
+            m_rectTransform = GetComponent<RectTransform>();
+
+            EventTrigger eventTrigger = GetComponent<EventTrigger>();
+            EventTrigger.Entry entryDrag = new EventTrigger.Entry();
+            entryDrag.eventID = EventTriggerType.Drag;
+            entryDrag.callback.AddListener(OnDrag);
+            eventTrigger.triggers.Add(entryDrag);
+
+            EventTrigger.Entry entryEndDrag = new EventTrigger.Entry();
+            entryEndDrag.eventID = EventTriggerType.EndDrag;
+            entryEndDrag.callback.AddListener(OnEndDrag);
+            eventTrigger.triggers.Add(entryEndDrag);
+
+            UpdateWindowSize();
+        }
+
+        private IEnumerator Start()
+        {
+            yield return new WaitForEndOfFrame();
+
+            UpdateWindowSize();
+            OnEndDrag(null);
+        }
+
+        private void UpdateWindowSize()
+        {
+            m_fullWindowSize = GetComponentInParent<Canvas>().GetComponent<RectTransform>().sizeDelta / 2;
+        }
+
+        private void Update()
+        {
+            if (m_isMoving)
+            {
+                m_moveTimer += Time.deltaTime;
+                m_rectTransform.anchoredPosition = Vector2.Lerp(m_startPosition, m_targetPosition, m_moveTimer / m_duration);
+
+                if (m_moveTimer >= m_duration)
+                {
+                    m_isMoving = false;
+                    m_rectTransform.anchoredPosition = m_targetPosition;
+                }
+            }
+        }
+
+        public bool IsMoving()
+        {
+            return m_isMoving;
+        }
+
+        private bool CantMove()
+        {
+            if(m_debuggerSizeHelper != null)
+            {
+                return IsMoving() || m_debuggerSizeHelper.IsFullScreen();
+            }
+            else
+            {
+                return IsMoving();
+            }
+        }
+
+        private void OnDrag(BaseEventData eventData)
+        {
+            if (CantMove())
+            {
+                return;
+            }
+
+            transform.position = Input.mousePosition;
+            m_isMoving = false;
+        }
+
+        private void OnEndDrag(BaseEventData eventData)
+        {
+            if (!m_moveToBorder)
+            {
+                m_targetPosition = m_rectTransform.anchoredPosition;
+                return;
+            }
+
+            if (CantMove())
+            {
+                return;
+            }
+
+            Vector2 position = m_rectTransform.anchoredPosition;
+
+            float[] borderDistance = new float[4];
+            borderDistance[0] = m_fullWindowSize.y - position.y;
+            borderDistance[1] = position.y + m_fullWindowSize.y;
+            borderDistance[2] = position.x + m_fullWindowSize.x;
+            borderDistance[3] = m_fullWindowSize.x - position.x;
+
+            int closestIndex = 0;
+            float closestDistance = borderDistance[closestIndex];
+            for (int i = 1; i < borderDistance.Length; i++)
+            {
+                if (closestDistance > borderDistance[i])
+                {
+                    closestIndex = i;
+                    closestDistance = borderDistance[i];
+                }
+            }
+
+            SetTargetPosition(closestIndex);
+        }
+
+        private void SetTargetPosition(int direction)
+        {
+            m_startPosition = m_rectTransform.anchoredPosition;
+            m_targetPosition = m_rectTransform.anchoredPosition;
+            switch (direction)
+            {
+                case 0:
+                    m_targetPosition.y = m_fullWindowSize.y - m_rectTransform.sizeDelta.y / 2;
+                    break;
+                case 1:
+                    m_targetPosition.y = -m_fullWindowSize.y + m_rectTransform.sizeDelta.y / 2;
+                    break;
+                case 2:
+                    m_targetPosition.x = -m_fullWindowSize.x + m_rectTransform.sizeDelta.x / 2;
+                    break;
+                case 3:
+                    m_targetPosition.x = m_fullWindowSize.x - m_rectTransform.sizeDelta.y / 2;
+                    break;
+            }
+
+            m_moveTimer = 0;
+            m_isMoving = true;
+        }
+    }
+}
---
+++ Plugins/Debugger/DebuggerSizeHelper.cs
@@ -0,0 +1,147 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+namespace TEDCore.Debugger
+{
+    public class DebuggerSizeHelper : MonoBehaviour
+    {
+        private const float FULL_SIZE_RATIO = 1.0f;
+
+        [SerializeField] private float m_duration = 0.1f;
+        [SerializeField] private GameObject m_dragImage;
+        [SerializeField] private GameObject m_fullScreen;
+
+        private RectTransform m_rectTransform;
+
+        private Vector2 m_startPosition;
+        private Vector2 m_targetPosition;
+
+        #region Screen
+        private Vector2 m_initSizeDelta;
+        private Vector2 m_fullSizeDelta;
+        private float m_pointerDownTime;
+        private Vector2 m_startSizeDelta;
+        private Vector2 m_targetSizeDelta;
+        private float m_sizeTimer;
+        private bool m_isSizing;
+        #endregion
+
+        private void Awake()
+        {
+            m_rectTransform = GetComponent<RectTransform>();
+
+            EventTrigger eventTrigger = GetComponent<EventTrigger>();
+            EventTrigger.Entry entryPointerDown = new EventTrigger.Entry();
+            entryPointerDown.eventID = EventTriggerType.PointerDown;
+            entryPointerDown.callback.AddListener(OnPointerDown);
+            eventTrigger.triggers.Add(entryPointerDown);
+
+            EventTrigger.Entry entryPointerUp = new EventTrigger.Entry();
+            entryPointerUp.eventID = EventTriggerType.PointerUp;
+            entryPointerUp.callback.AddListener(OnPointerUp);
+            eventTrigger.triggers.Add(entryPointerUp);
+
+            m_dragImage.SetActive(true);
+            m_fullScreen.SetActive(false);
+        }
+
+        private IEnumerator Start()
+        {
+            yield return new WaitForEndOfFrame();
+
+            Canvas canvas = GetComponentInParent<Canvas>();
+            Vector2 sizeDelta = canvas.GetComponent<RectTransform>().sizeDelta;
+
+            m_initSizeDelta = m_rectTransform.sizeDelta;
+            m_fullSizeDelta = sizeDelta * FULL_SIZE_RATIO;
+        }
+
+        private void Update()
+        {
+            if (m_isSizing)
+            {
+                m_sizeTimer += Time.deltaTime;
+                m_rectTransform.anchoredPosition = Vector2.Lerp(m_startPosition, m_targetPosition, m_sizeTimer / m_duration);
+                m_rectTransform.sizeDelta = Vector2.Lerp(m_startSizeDelta, m_targetSizeDelta, m_sizeTimer / m_duration);
+
+                if (m_sizeTimer >= m_duration)
+                {
+                    m_isSizing = false;
+                    m_rectTransform.anchoredPosition = m_targetPosition;
+                    m_rectTransform.sizeDelta = m_targetSizeDelta;
+
+                    if (m_targetSizeDelta == m_initSizeDelta)
+                    {
+                        m_dragImage.SetActive(true);
+                        m_fullScreen.SetActive(false);
+                    }
+                    else if (m_targetSizeDelta == m_fullSizeDelta)
+                    {
+                        m_fullScreen.SetActive(true);
+                    }
+                }
+            }
+        }
+
+        private bool IsSizing()
+        {
+            return m_isSizing;
+        }
+
+        public bool IsFullScreen()
+        {
+            return m_fullScreen.activeInHierarchy;
+        }
+
+        public void OnPointerDown(BaseEventData eventData)
+        {
+            if (IsSizing() || IsFullScreen())
+            {
+                return;
+            }
+
+            m_pointerDownTime = Time.realtimeSinceStartup;
+        }
+
+        public void OnPointerUp(BaseEventData eventData)
+        {
+            if (IsSizing() || IsFullScreen())
+            {
+                return;
+            }
+
+            float pointerUpTime = Time.realtimeSinceStartup;
+            if (pointerUpTime - m_pointerDownTime > 0.1f)
+            {
+                return;
+            }
+
+            m_dragImage.SetActive(false);
+
+            m_startPosition = m_rectTransform.anchoredPosition;
+            m_targetPosition = Vector2.zero;
+            m_startSizeDelta = m_initSizeDelta;
+            m_targetSizeDelta = m_fullSizeDelta;
+
+            m_sizeTimer = 0;
+            m_isSizing = true;
+        }
+
+        public void OnFullScreenCloseButtonClicked()
+        {
+            if (IsSizing())
+            {
+                return;
+            }
+
+            m_targetPosition = m_startPosition;
+            m_startPosition = Vector2.zero;
+            m_startSizeDelta = m_fullSizeDelta;
+            m_targetSizeDelta = m_initSizeDelta;
+
+            m_sizeTimer = 0;
+            m_isSizing = true;
+        }
+    }
+}
