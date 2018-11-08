fe2e8e9f0f7621f364358ae01214a681c5e9e15c
ted <ted10401@gmail.com>
2018-11-08 13:43:05 +0800

    Adjust the information in debugger profiler.

--- Tools/Debugger/Profiler/Scripts/ProfilerCanvas.cs
+++ Tools/Debugger/Profiler/Scripts/ProfilerCanvas.cs
@@ -7,33 +7,37 @@ namespace TEDCore.Debugger.Profiler
     {
         private const int HIGH_FPS = 60;
         private const int MIDIUM_FPS = 30;
-        private const string FPS_FORMAT = "Cur FPS: {0}\nMin FPS: {1}\nMax FPS: {2}\nAvg FPS: {3}";
+
+        private const string MINI_FORMAT = "FPS: {0}\nMono: {1}MB\nMemory: {2}MB";
+
+        private const string FULL_FPS_FORMAT = "Cur FPS: \t<color={0}>{1}</color>\nMin FPS: \t<color={2}>{3}</color>\nMax FPS: \t<color={4}>{5}</color>\nAvg FPS: \t<color={6}>{7}</color>";
+        private const string FULL_MONO_FORMAT = "Used Size: \t{0:0.0}MB\nHeap Size: \t{1:0.0}MB";
+        private const string FULL_MEMORY_FORMAT = "Allocated Size: \t\t{0:0.0}MB\nReserved Size: \t{1:0.0}MB";
+
+        private const string COLOR_GREEN_FORMAT = "#00ff00";
+        private const string COLOR_YELLOW_FORMAT = "#ffff00";
+        private const string COLOR_RED_FORMAT = "#ff0000";
 
         private const int MEGA_BYTE = 1048576;
 
-        [SerializeField] private bool m_updateColor;
         [SerializeField] private float m_updateDuration = 1.0f;
-        [SerializeField] private Image m_colorBackground;
-        [SerializeField] private Text m_fpsText;
-        [SerializeField] private Button m_resetButton;
 
-        private const string MONO_TOTAL_TEXT = "Mono Heap Size: {0:0.0}MB";
-        private const string MONO_USED_TEXT = "Mono Used Size: {0:0.0}MB";
-        private const string MEMORY_TOTAL_TEXT = "Total Reserved Memory Size: {0:0.0}MB";
-        private const string MEMORY_USED_TEXT = "Total Allocated Memory Size: {0:0.0}MB";
+        [Header("Mini Screen")]
+        [SerializeField] private bool m_updateColor;
+        [SerializeField] private Image m_colorBackground;
+        [SerializeField] private Text m_miniText;
+
         [Header("Full Screen")]
         [SerializeField] private Text m_fullScreenFpsText;
-        [SerializeField] private Text m_monoHeapSizeText;
-        [SerializeField] private Text m_monoUsedSizeText;
-        [SerializeField] private Slider m_monoSlider;
+        [SerializeField] private Text m_fullScreenMonoText;
         [SerializeField] private Button m_gcCollectButton;
-        [SerializeField] private Text m_totalReservedMemoryText;
-        [SerializeField] private Text m_totalAllocatedMemoryText;
-        [SerializeField] private Slider m_memorySlider;
+        [SerializeField] private Text m_fullScreenMemoryText;
         [SerializeField] private Button m_cleanButton;
 
-        private float m_cacheValue1;
-        private float m_cacheValue2;
+        private float m_monoUsedSize;
+        private float m_monoHeapSize;
+        private float m_memoryAllocatedSize;
+        private float m_memoryReservedSize;
 
         private ProfilerManager m_performanceManager;
         private Color m_color;
@@ -43,7 +47,6 @@ namespace TEDCore.Debugger.Profiler
             m_performanceManager = ProfilerManager.Instance;
             m_performanceManager.SetUpdateDuration(m_updateDuration);
             m_performanceManager.SetUpdateCallback(OnUpdate);
-            m_resetButton.onClick.AddListener(() => m_performanceManager.Reset());
             m_gcCollectButton.onClick.AddListener(() => System.GC.Collect());
             m_cleanButton.onClick.AddListener(() =>
             {
@@ -74,22 +77,49 @@ namespace TEDCore.Debugger.Profiler
                 m_color = Color.white;
             }
 
+            m_monoUsedSize = UnityEngine.Profiling.Profiler.GetMonoUsedSizeLong() / MEGA_BYTE;
+            m_monoHeapSize = UnityEngine.Profiling.Profiler.GetMonoHeapSizeLong() / MEGA_BYTE;
+            m_memoryAllocatedSize = UnityEngine.Profiling.Profiler.GetTotalAllocatedMemoryLong() / MEGA_BYTE;
+            m_memoryReservedSize = UnityEngine.Profiling.Profiler.GetTotalReservedMemoryLong() / MEGA_BYTE;
+
+            #region Mini Screen
             m_colorBackground.color = m_color * 0.5f;
-            m_fpsText.text = string.Format(FPS_FORMAT, m_performanceManager.FpsData.CurValue, m_performanceManager.FpsData.MinValue, m_performanceManager.FpsData.MaxValue, m_performanceManager.FpsData.AvgValue);
+            m_miniText.text = string.Format(MINI_FORMAT, m_performanceManager.FpsData.CurValue, m_monoUsedSize, m_memoryAllocatedSize);
+            #endregion
 
-            m_fullScreenFpsText.text = m_fpsText.text;
+            #region Mull Screen
+            m_fullScreenFpsText.text = GetFullScreenFpsText(m_performanceManager.FpsData);
+            m_fullScreenMonoText.text = string.Format(FULL_MONO_FORMAT, m_monoUsedSize, m_monoHeapSize);
+            m_fullScreenMemoryText.text = string.Format(FULL_MEMORY_FORMAT, m_memoryAllocatedSize, m_memoryReservedSize);
+            #endregion
+        }
 
-            m_cacheValue1 = UnityEngine.Profiling.Profiler.GetMonoHeapSizeLong();
-            m_cacheValue2 = UnityEngine.Profiling.Profiler.GetMonoUsedSizeLong();
-            m_monoHeapSizeText.text = string.Format(MONO_TOTAL_TEXT, m_cacheValue1 / MEGA_BYTE);
-            m_monoUsedSizeText.text = string.Format(MONO_USED_TEXT, m_cacheValue2 / MEGA_BYTE);
-            m_monoSlider.value = m_cacheValue2 / m_cacheValue1;
+        private string GetFullScreenFpsText(ProfilerData profilerData)
+        {
+            return string.Format(FULL_FPS_FORMAT
+                                 , GetFpsColorFormat(profilerData.CurValue), profilerData.CurValue
+                                 , GetFpsColorFormat(profilerData.MinValue), profilerData.MinValue
+                                 , GetFpsColorFormat(profilerData.MaxValue), profilerData.MaxValue
+                                 , GetFpsColorFormat(profilerData.AvgValue), profilerData.AvgValue);
+        }
 
-            m_cacheValue1 = UnityEngine.Profiling.Profiler.GetTotalReservedMemoryLong();
-            m_cacheValue2 = UnityEngine.Profiling.Profiler.GetTotalAllocatedMemoryLong();
-            m_totalReservedMemoryText.text = string.Format(MEMORY_TOTAL_TEXT, m_cacheValue1 / MEGA_BYTE);
-            m_totalAllocatedMemoryText.text = string.Format(MEMORY_USED_TEXT, m_cacheValue2 / MEGA_BYTE);
-            m_memorySlider.value = m_cacheValue2 / m_cacheValue1;
+        private string GetFpsColorFormat(float fps)
+        {
+            string color = string.Empty;
+            if(fps >= HIGH_FPS)
+            {
+                color = COLOR_GREEN_FORMAT;
+            }
+            else if(fps >= MIDIUM_FPS)
+            {
+                color = COLOR_YELLOW_FORMAT;
+            }
+            else
+            {
+                color = COLOR_RED_FORMAT;
+            }
+
+            return color;
         }
     }
 }
