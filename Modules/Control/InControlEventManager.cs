748e0d2e455347944d56dd37ebd599f33728a65b
ted <ted10401@gmail.com>
2018-12-13 11:28:47 +0800

    Add the control module

---
+++ Modules/Control/InControlEventManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using TEDCore.Event;
+using InControl;
+
+namespace TEDCore.Control
+{
+    public class InControlEventManager : MonoBehaviour
+    {
+        private KeyboardVirtualDevice m_virtualDevice;
+        private InputDevice m_inputDevice;
+        private EventManager m_eventManager;
+        private EventListener m_eventListener;
+
+        private void Awake()
+        {
+            CacheEventManager();
+        }
+
+        private void OnEnable()
+        {
+            m_virtualDevice = new KeyboardVirtualDevice();
+            InputManager.OnSetup += () => InputManager.AttachDevice(m_virtualDevice);
+        }
+
+        private void OnDisable()
+        {
+            InputManager.DetachDevice(m_virtualDevice);
+            m_virtualDevice = null;
+        }
+
+        private void Update()
+        {
+            m_inputDevice = InputManager.ActiveDevice;
+
+            CacheEventManager();
+            m_eventManager.SendEvent((int)InputControlType.LeftStickX, m_inputDevice.LeftStickX);
+            m_eventManager.SendEvent((int)InputControlType.LeftStickY, m_inputDevice.LeftStickY);
+
+            m_inputDevice = null;
+        }
+
+        private void CacheEventManager()
+        {
+            if (m_eventManager != null)
+            {
+                return;
+            }
+
+            m_eventManager = EventManager.Instance;
+        }
+    }
+}
---
+++ Modules/Control/KeyboardVirtualDevice.cs
@@ -0,0 +1,68 @@
+﻿using InControl;
+using UnityEngine;
+
+namespace TEDCore.Control
+{
+    public class KeyboardVirtualDevice : InputDevice
+    {
+        private const float SENSITIVITY = 0.1f;
+
+        private float m_kx;
+        private float m_ky;
+
+        public KeyboardVirtualDevice() : base("Keyboard Controller")
+        {
+            AddControl(InputControlType.LeftStickLeft, "Left Stick Left");
+            AddControl(InputControlType.LeftStickRight, "Left Stick Right");
+            AddControl(InputControlType.LeftStickUp, "Left Stick Up");
+            AddControl(InputControlType.LeftStickDown, "Left Stick Down");
+        }
+
+        public override void Update(ulong updateTick, float deltaTime)
+        {
+            var leftStickVector = GetVectorFromKeyboard(deltaTime, true);
+            UpdateLeftStickWithValue(leftStickVector, updateTick, deltaTime);
+
+            Commit(updateTick, deltaTime);
+        }
+
+
+        private Vector2 GetVectorFromKeyboard(float deltaTime, bool smoothed)
+        {
+            if (smoothed)
+            {
+                m_kx = ApplySmoothing(m_kx, GetXFromKeyboard(), deltaTime, SENSITIVITY);
+                m_ky = ApplySmoothing(m_ky, GetYFromKeyboard(), deltaTime, SENSITIVITY);
+            }
+            else
+            {
+                m_kx = GetXFromKeyboard();
+                m_ky = GetYFromKeyboard();
+            }
+            return new Vector2(m_kx, m_ky);
+        }
+
+        private float GetXFromKeyboard()
+        {
+            return UnityEngine.Input.GetAxis("Horizontal"); ;
+        }
+
+        private float GetYFromKeyboard()
+        {
+            return UnityEngine.Input.GetAxis("Vertical");
+        }
+
+        private float ApplySmoothing(float lastValue, float thisValue, float deltaTime, float sensitivity)
+        {
+            sensitivity = Mathf.Clamp(sensitivity, 0.001f, 1.0f);
+
+            if (Mathf.Approximately(sensitivity, 1.0f))
+            {
+                return thisValue;
+            }
+
+            return Mathf.Lerp(lastValue, thisValue, deltaTime * sensitivity * 100.0f);
+        }
+    }
+}
+
